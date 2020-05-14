01fd4d26ee2722c0e9ba0aab5f237a5e0cd004e7
Vondru Nicolas, LA4, BUZ <nicolas.vondru@buhlergroup.com>
2020-05-14 14:16:17 +0200

    some changes >.<

---
+++ src/components/NavBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="navbar" class="navbar"></div>
+</template>
+
+<style scoped>
+.navbar {
+  height: 80px;
+  background-color: lightblue;
+  position: fixed;
+}
+</style>
---
+++ src/views/Products.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="products">
+    <ProductsContainer></ProductsContainer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {}
+};
+</script>
